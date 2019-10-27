2fa8f6c814638f0dede0e9c32cd7a8bacfa60b50
202201 <kryska.jakub@gmail.com>
2019-10-27 17:15:43 +0100

    Basic structure for Command design patter

---
+++ src/main/java/edu/kis/powp/jobs2d/commander/SetPositionCommand.java
@@ -0,0 +1,10 @@
+package edu.kis.powp.jobs2d.commander;
+
+public class SetPositionCommand implements DriverCommand {
+    private int x;
+    private int y;
+    @Override
+    public void execute() {
+
+    }
+}
